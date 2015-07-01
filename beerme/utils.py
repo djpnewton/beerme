b06564f897c5e63a6af6229aa5234ef5de4820a7
Daniel Newton <djpnewton@gmail.com>
2015-07-01 19:37:02 +1200

    add multiple email addresses for beer notifications

--- beerme/utils.py
+++ beerme/utils.py
@@ -15,24 +15,35 @@ def qrcode_png_buffer(qr):
 def send_email(config, email, subject, body):
     print 'sending email to %s (%s)' % (email, subject)
     if config.email.use_mandrill:
+        if isinstance(email, basestring):
+            to=[{'email': email}]
+        else:
+            to = []
+            for addr in email:
+                to.append({'email': addr})
         import flask
         from flask.ext.mandrill import Mandrill
 
         mandrill = Mandrill(flask.current_app)
         mandrill.send_email(
             subject=subject,
-            to=[{'email': email}],
+            to=to,
             text=body
         )
     else:
         import smtplib
         from email.mime.text import MIMEText
 
+        if isinstance(email, basestring):
+            to=email
+        else:
+            to = ', '.join(email)
+
         # Create a text/plain message
         msg = MIMEText(body)
         msg['Subject'] = subject
         msg['From'] = config.email.from_
-        msg['To'] = email
+        msg['To'] = to
 
         # Send the message via our own SMTP server, but don't include the
         # envelope header.
@@ -47,5 +58,10 @@ def send_email(config, email, subject, body):
 def send_email_beer_alert(config, beer):
     subject = 'beerme order'
     body = """%s at table %s has ordered a %s""" % (beer.name, beer.table, beer.brew)
-    send_email(config, config.email.beer_alert, subject, body)
+    send_email(config, config.email.beer_alert.split(), subject, body)
+
+def send_email_test(config):
+    subject = 'beerme test email'
+    body = 'bleh'
+    send_email(config, config.email.beer_alert.split(), subject, body)
 
